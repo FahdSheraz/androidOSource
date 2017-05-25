645430af83cfa7e245907b3f902db49943b684d2
Fahd Sheraz <fahd.sheraz@bournville.ac.uk>
2017-05-25 11:20:52 -0700

    Java Arrays and ArrayList

--- JavaArrays/src/com/gohooljava/com/Main.java
+++ JavaArrays/src/com/gohooljava/com/Main.java
@@ -20,6 +20,46 @@ public class Main {
 
 
 
+    ArrayList animals = new ArrayList();
+    animals.add("Pig");
+    animals.add("Cow");
+    animals.add("Parrot");
+
+    //How to get items to print ou
+    System.out.println(animals.get(0)); // output == Pig
+    System.out.println(animals.get(1)); // output == Cow
+
+    //We can also remove and do all sort of things wiht ArrayLists
+     animals.remove(0); //removes the first item in our arraylist
+
+     //we can add items on the fly
+     animals.add(0, "Dog"); //add Dog on index 0
+
+     //We can remove by calling the object name
+     animals.remove("Cow"); //will remove "Cow"
+
+  	//Find the size of the arraylist
+     animals.size();
+    //How to iterate through our arrayList
+
+
+     //use "contains()" method to do some checking logic
+     if (animals.contains("Cow")) {
+
+     	 //Do something here!
+     	
+     }
+
+    for (int i=0; i<animals.size(); i++ ) {
+    	System.out.println("Animals:" +animals.get(i)); // get all animals in our arraylist
+    	
+    }
+
+    //Enhanced for-loop
+    for(Object animal : animals) {
+
+    		System.out.println("Animals:" +animal; // get all animals in our arraylist
+    }
 
 
 
