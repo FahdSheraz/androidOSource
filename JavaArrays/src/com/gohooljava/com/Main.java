4038982f20ba9fc3e1f1f4dd0dca2c7c0d44de4c
Fahd Sheraz <fahd.sheraz@bournville.ac.uk>
2017-05-25 11:07:27 -0700

    Arrays

---
+++ JavaArrays/src/com/gohooljava/com/Main.java
@@ -0,0 +1,60 @@
+package com.gohooljava.com;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int myArray[] = {1,3,5};
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("MyArray" + myArray[i]);
+
+        }
+
+
+//      
+
+
+
+
+
+
+
+
+
+
+//
+//        int[] myArray = {1, 2, 3, 4, 5, 6, 7};
+//        String[] myStringArray = {"Hello", "Hi", "What"};
+//
+//
+//        int[] newArray = new int[5];
+//        String[] newStringArray = new String[5];
+//
+//        newArray[0] = 3;
+//        newArray[1] = 4;
+//        newArray[2] = 6;
+//        newArray[3] = 1;
+//        newArray[4] = 12;
+//
+//        newStringArray[0] = "Marcos";
+//        newStringArray[1] = "James";
+//        newStringArray[2] = "Jason";
+//        newStringArray[3] = "Laura";
+//        newStringArray[4] = "Bonni";
+//
+//
+//
+//
+//
+//        //Arrays have length.
+//        for (int i = 0; i < newStringArray.length ; i++) {
+//            System.out.println("Item  " + newStringArray[i]);
+//        }
+//
+
+    }
+}
