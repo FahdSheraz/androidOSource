152931bc3bc373698620120ffcb653838cf594fc
Fahd Sheraz <fahd.sheraz@bournville.ac.uk>
2017-05-25 10:46:21 -0700

    HelloWorldApp - Variables and More

---
+++ HelloworldApp/src/com/gohooljava/com/Main.java
@@ -0,0 +1,39 @@
+package com.gohooljava.com;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //variables  - little empty buckets
+       //String myName = "Paulo";
+        String lastName = "Dichone";
+
+
+        //Integers
+        int myAge = 31;
+
+        //Byte - small type
+        byte myByte = 127;
+
+        //Short 2x byte
+        short myShort = 128;
+
+        //Float 1.34, 3.14,....
+        float piNum = 3.14f;
+
+        // Double
+        double myDouble = 23.89;
+
+        // Char == character ( &, $, @ ., 0..)
+        char myChar = 'h';
+
+        //Boolean = true or false, 1 or 0
+        boolean myBool = true;
+
+
+        System.out.println("My char is: " + myChar + " and my bool is " + myBool);
+
+
+	    //System.out.println("Hi my name is " + lastName + " and I am " + myAge + " years old." );
+    }
+}
